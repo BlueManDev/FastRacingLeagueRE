3c6bf532cdfab05736bbdb8ad11e3a851c73fc87
BlueManDev <96463917+BlueManDev@users.noreply.github.com>
2026-04-12 12:14:46 +1000

    Update Entity.h

--- EngineCode/Entities/Entity.h
+++ EngineCode/Entities/Entity.h
@@ -1,11 +1,22 @@
 #pragma once
 
 
+extern int gUIdCount;
+
 class CEntity
 {
 public:
 	CEntity();
 
 public:
+	const char* name;
+	const char* baseName;
+	const char* baseClass;
+
+	float priority;
+
 	int uId;
+
+private:
+	bool deleteEntity;
 };
