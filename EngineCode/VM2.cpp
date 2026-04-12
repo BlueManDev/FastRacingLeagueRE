f2eba4a849f48d7badad527f91cce3f91cd51c20
BlueManDev <96463917+BlueManDev@users.noreply.github.com>
2026-04-12 12:13:45 +1000

    Update VM2.cpp

--- EngineCode/VM2.cpp
+++ EngineCode/VM2.cpp
@@ -1,7 +1,5 @@
 #include "VM2.h"
 
-#include "Temporary.h"
-
 #include "WiiSDKFunctions/osD.h"
 
 #include <string>
@@ -20,22 +18,21 @@ void CVM::LoadScriptGuarded(const char* scriptPath, int param_2)
 		{
 			if (*scriptPath == '/')
 			{
-				AutoStruct1* v0 = gAutoClass2->Load(scriptPath, 0, 32);
-				if (v0->magicString == 0x53485641) // SHVA
+				CResource* scriptResource = gResourceMgr->Load(scriptPath, 0, 32);
+				if (scriptResource->headerId == 0x53485641) // "SHVA"
 				{
-					if (v0->version != 0x132b5a0)
-						OSPanic(__FILE__, __LINE__, "%s: illegal version (expected %d, got %d)", scriptPath, 0x132b5a0, v0->magicString); // This should be on line 632 according to the original code
+					if (scriptResource->version != 0x132b5a0)
+						OSPanic(__FILE__, __LINE__, "%s: illegal version (expected %d, got %d)", scriptPath, 0x132b5a0, scriptResource->headerId); // This should be on line 632 according to the original code
 
 					// Loads each script inside the .vma archive???
-					for (int i = v0->field_0x8; i != 0; i = (i + i + 4)) // what?
+					for (int i = scriptResource->field_0x8; i != 0; i = (i + i + 4)) // what?
 					{
-						// CVM function call goes here, it basically does similar thing to this function but for SHVM scripts
-
+						// LoadScript__Unk is called here with some weird pointer bullshit
 					}
 				}
 				else
 				{
-					// Same function call without the for loop bullshit
+					LoadScript__Unk(scriptPath, scriptResource, 0, param_2);
 				}
 			}
 		}
