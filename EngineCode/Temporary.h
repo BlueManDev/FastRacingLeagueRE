9dba3c63d5d23571f17ea614f23c1af158bbde78
BlueManDev <96463917+BlueManDev@users.noreply.github.com>
2026-04-12 12:12:47 +1000

    Update Temporary.h

--- EngineCode/Temporary.h
+++ EngineCode/Temporary.h
@@ -2,22 +2,11 @@
 
 // Stuff that doesn't really have a location yet
 
-// Some sort of resource struct/class, will probably dive back into The Touryst eventually to see if there's any symbols related to this
-struct AutoStruct1
-{
-	// Temporary names based on their use in CVM::LoadScriptGuarded
-	int magicString;
-	int version;
-	int field_0x8;
-};
-
-// Another resource class, presumably a resource manager
-class AutoClass2
+class CVector
 {
 public:
-	AutoClass2() = default;
+	CVector(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
 
-	AutoStruct1* Load(const char* resourcePath, int param_2, int param_3);
+public:
+	float x, y, z;
 };
-
-AutoClass2* gAutoClass2;
